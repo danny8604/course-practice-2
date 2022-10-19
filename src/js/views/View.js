115085daddd7ee25fe50a2d887792ee6c90d4c44
u0454 <zx320320@gmail.com>
2022-10-20 05:36:11 +0800

    modify fraction-10

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -97,6 +97,6 @@ export default class View {
           </div>
                 `;
     this._clear();
-    this._parentEl.insertAdjacentHTML('afterbegin', markup);
+    // this._parentEl.insertAdjacentHTML('afterbegin', markup);
   }
 }
