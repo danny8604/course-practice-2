5b9bcb87345f0d2266a9ea9e216a31312069ca23
u0454 <zx320320@gmail.com>
2022-10-20 05:45:55 +0800

    modify fraction-10

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -84,19 +84,19 @@ export default class View {
     this._clear();
     this._parentEl.insertAdjacentHTML('afterbegin', markup);
   }
-  // = this._message
-  // renderMessage(message) {
-  //   const markup = `
-  //         <div class="message">
-  //                 <div>
-  //                   <svg>
-  //                     <use href="${icons}#icon-smile"></use>
-  //                   </svg>
-  //                 </div>
-  //                 <p>${message}</p>
-  //         </div>
-  //               `;
-  //   this._clear();
-  //   // this._parentEl.insertAdjacentHTML('afterbegin', markup);
-  // }
+
+  renderMessage(message = this._message) {
+    const markup = `
+          <div class="message">
+                  <div>
+                    <svg>
+                      <use href="${icons}#icon-smile"></use>
+                    </svg>
+                  </div>
+                  <p>${message}</p>
+          </div>
+                `;
+    this._clear();
+    this._parentEl.insertAdjacentHTML('afterbegin', markup);
+  }
 }
